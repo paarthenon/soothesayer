749ba66494a6bcbf4d85b20626c4eaedce880934
P <paarthk+gh@gmail.com>
2022-07-17 16:41:29 -0400

    included build

--- src/ui/AboutPage.tsx
+++ src/ui/AboutPage.tsx
@@ -4,7 +4,7 @@ import {Link} from "./Link";
 
 export const AboutPage = () => (
     <Box>
-        <Text>A game made for GMTK 2022.</Text>
+        <Text>A game made for GMTK 2022 by Paarth and Boibi</Text>
 
         <Heading>Credits & Attributions</Heading>
         <Text>
@@ -15,7 +15,7 @@ export const AboutPage = () => (
 
         <List>
             <ListItem>
-                Person — Work
+                https://fineartamerica.com/featured/1-portrait-of-jeanne-wenz-henri-de-toulouse-lautrec.html
             </ListItem>
         </List>
 
--- src/ui/MainMenu.tsx
+++ src/ui/MainMenu.tsx
@@ -1,9 +1,10 @@
-import {Box, Heading, Text, List, ListItem, useDisclosure} from '@chakra-ui/react';
+import {Box, Heading, Text, List, ListItem, useDisclosure, Image} from '@chakra-ui/react';
 import {View} from 'core/view';
 import {useDispatch} from 'react-redux';
 import {Action} from 'redux/actions';
 import {useAppState} from "redux/hooks";
 import {Link} from 'ui/Link';
+import cover from 'img/cover.png';
 
 export interface MainMenuProps { }
 export const MainMenu = ({ }: MainMenuProps) => {
@@ -15,7 +16,8 @@ export const MainMenu = ({ }: MainMenuProps) => {
     }
     return (
         <Box>
-            <Heading>Soothe Sayer</Heading>
+            <Image src={cover} />
+
             <Text>
                 Fate can be cruel, but people don't tip for bad news.
                 {/* Find what fate has in store, then tell it to fuck off */}
