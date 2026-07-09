baa0d31973a1c6640c6a46808c3e5abbd575972d
P <paarthk+gh@gmail.com>
2026-07-08 20:08:10 -0400

    Update serve | clean up readme

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-    base: 'https://paarthenon.github.io/soothesayer/',
+    base: '/',
     resolve: {
         tsconfigPaths: true,
     },
